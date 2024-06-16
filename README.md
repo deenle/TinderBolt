5937095140b98f26b013ed31e8b8729064bfc3cc
Tim B <dleenly@gmail.com>
2024-06-17 01:01:34 +0200

    Bot /start abilities v1

---
+++ README.md
@@ -0,0 +1,49 @@
+# Telegram Bot with ChatGPT for Dating practicing
+
+---
+
+## Bot will be capable of:
+* Having a dialog;
+* Practicing in dates;
+* Generating cool descriptions for dating profiles on the sites;
+* Giving tips for chatting on the dating site.
+
+---
+
+## Setup
+
+Add environment variables to OS or to 'Run Configuration' in IntelliJ IDEA.
+* **TG_BOT_NAME** - for bot name as `"full_name_of_chat_bot"`;
+* **TG_BOT_TOKEN** - for bot token;
+* **OPENAI_TOKEN** - for OpenAI token.
+
+---
+### Credits
+* [JavaRush](https://javarush.com)
+
+* [TelegramBots](https://github.com/rubenlagus/TelegramBots)
+
+* OpenAI
+
+---
+
+### License
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- src/main/java/com/javarush/telegram/TinderBoltApp.java
+++ src/main/java/com/javarush/telegram/TinderBoltApp.java
@@ -9,12 +9,10 @@ import org.telegram.telegrambots.meta.api.objects.*;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import java.util.ArrayList;
-
 public class TinderBoltApp extends MultiSessionTelegramBot {
-    public static final String TELEGRAM_BOT_NAME = "bot-name"; //TODO: добавь имя бота в кавычках
-    public static final String TELEGRAM_BOT_TOKEN = "bot-token"; //TODO: добавь токен бота в кавычках
-    public static final String OPEN_AI_TOKEN = "chat-gpt-token"; //TODO: добавь токен ChatGPT в кавычках
+    public static final String TELEGRAM_BOT_NAME = System.getenv("TG_BOT_NAME");
+    public static final String TELEGRAM_BOT_TOKEN = System.getenv("TG_BOT_TOKEN");
+    public static final String OPEN_AI_TOKEN = System.getenv("OPENAI_TOKEN");
 
     public TinderBoltApp() {
         super(TELEGRAM_BOT_NAME, TELEGRAM_BOT_TOKEN);
@@ -23,6 +21,36 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
     @Override
     public void onUpdateEventReceived(Update update) {
         //TODO: основной функционал бота будем писать здесь
+        DialogMode currentMode;
+        String fileName;
+        String message = getMessageText();
+
+        if(message.startsWith("/start")) {
+            currentMode = DialogMode.MAIN;
+            fileName = currentMode.name().toLowerCase();
+            sendPhotoMessage(fileName);
+            sendTextMessage(loadMessage(fileName));
+            return;
+        }
+
+        // Bot menu
+        showMainMenu(DialogMode.MAIN.name(), "/start",
+                DialogMode.PROFILE.name(), "/profile",
+                DialogMode.OPENER.name(), "/opener",
+                DialogMode.MESSAGE.name(), "/message",
+                DialogMode.DATE.name(), "/date",
+                DialogMode.GPT.name(), "/gpt");
+
+        sendTextButtonsMessage("Push the Button",
+                "START", "start_btn",
+                        "STOP", "stop_btn");
+//        sendTextMessage("Hello from TinderBolt");
+        if (getCallbackQueryButtonKey().equalsIgnoreCase("stop_btn")) {
+            sendTextMessage("We are stopping");
+        } else if (getCallbackQueryButtonKey().equalsIgnoreCase("start_btn")) {
+            sendTextMessage("Let's continue");
+        }
+
 
     }
 
